7609f6584e610b210023804d05ecd3009b17b3ea
Joanne Tan <joanne.tan@bosch.com>
2020-12-01 11:20:15 +0800

    js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $("#living-window").click(function(){
+    alert("hello");
+    $("#living-window-off").hide();
+    $("#living-window-on").show();
+    $("#living-fan-off").show();
+    $("#living-fan-on").hide();
+    $("#living-ac-off").show();
+    $("#living-ac-on").hide();
+  });
+});
